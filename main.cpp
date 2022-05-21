ec7eb4ad3798572e884d6f8f0f81670465ef2427
liuvaneshka <46787523+liuvaneshka@users.noreply.github.com>
2022-05-20 21:11:02 -0300

    prueba de archivos

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 #include "Menu.h"
 #include "Escritor.h"
 #include "Cuento.h"
+#include "Archivo.h"
 
 using namespace  std;
 
 int main() {
+
+    string archivo_lecturas = "Lecturas.txt";
+    string archivo_autores = "Autores.txt";
+
+    Archivo ar_lec(archivo_lecturas), ar_au(archivo_autores);
+    //ar_lec.parserdatos();
+    cout << "Escritores: " <<  endl;
+    ar_lec.mostrar_datos();
+    cout << "Autores: " <<  endl;
+    ar_au.mostrar_datos();
 
     return 0;
 }
