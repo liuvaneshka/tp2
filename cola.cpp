303f351bee35417e4f6a5a0547e6ab5a3497a770
Abril Giordano Hoyo <91142344+AbiiGiordano@users.noreply.github.com>
2022-05-25 13:45:59 -0300

    Implementacion cola

---
+++ cola.cpp
@@ -0,0 +1,38 @@
+#include "cola.h"
+#include <iostream>
+
+Cola::Cola(){
+    primero = 0;
+    ultimo = 0;
+}
+
+void Cola::encolar(Dato dato){
+    Nodo* nuevo = new Nodo(dato);
+    if (primero)
+        ultimo -> cambiar_siguiente(nuevo);
+    else
+        primero = nuevo;
+    ultimo = nuevo;
+}
+
+void Cola::desencolar(){
+    Nodo* borrar = primero;
+    primero = primero -> obtener_siguiente();         // Si el primero quedo apuntando a null, entonces eliminé el ultimo elemento y ultimo tambien apunta a null
+    if (! primero)
+        ultimo = 0;
+    borrar = 0;
+    delete borrar;
+}
+
+bool Cola::vacia(){
+    return primero == 0;
+}
+
+Dato Cola::consultar(){
+    return primero -> obtener_dato();
+}
+
+Cola::~Cola(){
+    while (! vacia())
+        desencolar();
+}
--- main.cpp
+++ main.cpp
@@ -1,55 +1,20 @@
 #include <iostream>
-#include "lista.h"
+#include "cola.h"
 
-using namespace std;
+using  namespace std;
 
 int main(){
-    Lista <int> lista;
+    Cola cola;
 
-    lista.alta(5, 1);
-    lista.alta(8, 2);
-    lista.alta(3, 2);
-    lista.alta(9, 1);
+    cola.encolar(1);
+    cola.encolar(2);
+    cola.encolar(3);
+    cola.encolar(4);
+    cout << "El frente de la cola es: " << cola.consultar() << endl;
 
-    for (int i = 1; i <= lista.obtener_tamanio() - 1; i++){
-        cout << lista.consultar(i) << " - ";
+    while (! cola.vacia()){
+        cola.desencolar();
+        cout << "El nuevo frente de la cola es: " << cola.consultar() << endl;
     }
-    cout << lista.consultar(lista.obtener_tamanio()) << endl;
-
-    lista.baja(4);
-    cout << "Luego de la baja en la ultima posicion..." << endl;
-    lista.baja(1);
-    cout << "Luego de la baja en la primera posicion..." << endl;
-
-
-    for (int i = 1; i <= lista.obtener_tamanio() - 1; i++){
-        cout << lista.consultar(i) << " - ";
-    }
-    cout << lista.consultar(lista.obtener_tamanio()) << endl;
-
-    cout << '\n';
-
-    Lista <char> lista1;
-    lista1.alta('o', 1);
-    lista1.alta('l', 2);
-    lista1.alta('a', 3);
-    lista1.alta('h', 1);
-
-    for (int i = 1; i <= lista1.obtener_tamanio() - 1; i++){
-        cout << lista1.consultar(i) << " - ";
-    }
-    cout << lista1.consultar(lista1.obtener_tamanio()) << endl;
-
-    lista1.baja(1);
-    cout << "Luego de la baja en la primera posicion..." << endl;
-    lista1.baja(2);
-    cout << "Luego de la baja en la nueva segunda posicion..." << endl;
-
-
-    for (int i = 1; i <= lista1.obtener_tamanio() - 1; i++){
-        cout << lista1.consultar(i) << " - ";
-    }
-    cout << lista1.consultar(lista1.obtener_tamanio()) << endl;
-
     return 0;
 }
