73a9037eca1ae9cf8ad878c7dfbf365c989d7154
tiagosandoval <104266806+tiagosandoval@users.noreply.github.com>
2022-05-26 20:01:06 -0300

    Update CONSTANTES.h

--- CONSTANTES.h
+++ CONSTANTES.h
@@ -1,3 +1,10 @@
 
 enum generos {DRAMA, COMEDIA, FICCION, SUSPENSO,
               TERROR, ROMANTICA, HISTORICA};    // HISTORICA = 6
+
+string const POEMA = "P";
+string const CUENTO = "C";
+string const NOVELA = "N";
+
+int const DESCONOCIDO = -1;
+
