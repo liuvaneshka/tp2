051c5bc744d701ff1d4c2feed3f36899617fd894
liuvaneshka <46787523+liuvaneshka@users.noreply.github.com>
2022-06-02 07:19:10 -0300

    colores

--- CONSTANTES.h
+++ CONSTANTES.h
@@ -16,4 +16,13 @@ const int DESCONOCIDO = -1;
 const string ARCHIVO_LECTURAS = "Lecturas.txt";
 const string ARCHIVO_ESCRITORES = "Autores.txt";
 
+
+//colores
+const string NEGRO = "\e[1;30m";
+const string ROJO = "\e[1;31m";
+const string AZUL = "\e[1;34m";
+const string VERDE = "\e[1;32m";
+const string AMARILLO = "\e[1;33m";
+const string VIOLETA = "\e[1;35m";
+
 #endif // __CONSTANTES_H__
