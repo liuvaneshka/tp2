5f544c04c45f488ba479da402f042b6dd4c3374b
Abril Giordano Hoyo <91142344+AbiiGiordano@users.noreply.github.com>
2022-06-02 12:02:23 -0300

    Imprimir string de genero

--- Novela.cpp
+++ Novela.cpp
@@ -13,7 +13,28 @@ generos Novela::obtener_genero(){
 
 void Novela::mostrar_lectura(){
     Lectura::mostrar_lectura();
-    cout << "Género: " << obtener_genero() << "\n" << endl;
+    cout << "Género: " << string_genero(obtener_genero()) << "\n" << endl;
+}
+
+string Novela::string_genero(generos genero){
+    string cadena_genero;
+    switch(genero){
+        case 1:
+            cadena_genero = "DRAMA"; break;
+        case 2:
+            cadena_genero = "COMEDIA";break;
+        case 3:
+            cadena_genero = "FICCION"; break;
+        case 4:
+            cadena_genero = "SUSPENSO"; break;
+        case 5:
+            cadena_genero = "TERROR"; break;
+        case 6:
+            cadena_genero = "ROMANTICA"; break;
+        case 7:
+            cadena_genero = "HISTORICA"; break;
+    }
+    return cadena_genero;
 }
 
 void Novela::imprimir_novela_genero(generos genero){
