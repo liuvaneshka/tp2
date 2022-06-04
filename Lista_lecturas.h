4959f3da1685054d6d8a41e349d98085db36812d
Abril Giordano Hoyo <91142344+AbiiGiordano@users.noreply.github.com>
2022-06-03 21:45:57 -0300

    Update Lista_lecturas.h

--- Lista_lecturas.h
+++ Lista_lecturas.h
@@ -9,8 +9,8 @@ class Lista_lecturas{
 private:
     Lista<Lectura*> lista_lecturas;
 
-    //PRE: el objeto lectura debe ser un objeto valido
-    //POS: Determina la posicion de una lectura en la lista segun su anio de publicacion
+    //PRE: El objeto lectura debe ser un objeto valido.
+    //POS: Determina la posicion de una lectura en la lista segun su anio de publicacion.
     int posicion_segun_anio(Lectura* lectura);
 
 public:
@@ -23,7 +23,7 @@ public:
     ~Lista_lecturas();
 
     //PRE: El objeto Lista_lecturas debe ser un objeto válido.
-    //POST: Dirige al cursor a la primera posicion
+    //POST: Dirige al cursor a la primera posicion.
     void inicializar();
 
     //PRE: El objeto Nodo debe ser un objeto válido.
@@ -42,7 +42,7 @@ public:
     //POST: Agrega la lectura al final de la lista.
     void alta(Lectura* lectura);
 
-    //PRE: La lista no puede estar vacía. 1 <= pos <= obtener_tamanio()
+    //PRE: La lista no puede estar vacía. 1 <= pos <= obtener_tamanio().
     //POST: Elimina el dato que se encuentra en la posicion indicada.
     void baja(int posicion);
 
@@ -55,32 +55,32 @@ public:
     Lectura* consultar(int posicion);
 
     //PRE: -
-    //POS: Devuelve el indice (numero entero) de la lista de la lectura que se quiere rastrear
+    //POS: Devuelve el indice (numero entero) de la lista de la lectura que se quiere rastrear.
     int rastrear(string titulo);
 
     //PRE: -
-    //POS: Sortea un numero (1 <= num_random <= obtener_tamanio()) e imprime los datos de la lectura cuyo indice es el
-    //      numero sorteado
+    /*POS: Sortea un numero (1 <= num_random <= obtener_tamanio()) e imprime los datos de la lectura cuyo indice es el
+            numero sorteado. */
     void sortear();
 
     //PRE: lista_lecturas.vacia() = false
     //POS: Muestra el titulo de las lecturas
     void listar();
 
-    //PRE: lista_lecturas.vacia() = false
-    //POS: Muestra la informacion de las lecturas que pertenecen a la lista
+    //PRE: lista_lecturas.vacia() = false.
+    //POS: Muestra la informacion de las lecturas que pertenecen a la lista.
     void listar_titulos_lecturas();
 
-    //PRE: lista_lecturas.vacia() = false, tienen que ser anios validos
-    //POS: Muestra la informacion de las lecturas publicados entre dos anios particulares (siempre que pertenezcan a la lista)
+    //PRE: lista_lecturas.vacia() = false, tienen que ser anios validos.
+    //POS: Muestra la informacion de las lecturas publicados entre dos anios particulares (siempre que pertenezcan a la lista).
     void listar_entre_anios(int desde, int hasta);
 
-    //PRE: lista_lecturas.vacia() = false
-    //POS: Muestra la informacion de las lecturas escritos por un escritor en particular (siempre que pertenezcan a la lista)
+    //PRE: lista_lecturas.vacia() = false.
+    //POS: Muestra la informacion de las lecturas escritos por un escritor en particular (siempre que pertenezcan a la lista).
     void listar_por_escritor(string nombre_escritor);
 
-    //PRE: lista_lecturas.vacia() = false
-    //POS: Muestra la informacion de las Novelas de un genero en paricular (siempre que pertenezcan a la lista)
+    //PRE: lista_lecturas.vacia() = false.
+    //POS: Muestra la informacion de las Novelas de un genero en paricular (siempre que pertenezcan a la lista).
     void listar_por_genero(generos genero);
 };
 #endif // __LISTA_LECTURAS_H__
